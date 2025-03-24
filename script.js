e2ba2af496e875c4914c403248a6ab935023d3c8
Gabriel-Debian <106034246+gmjohnson683@users.noreply.github.com>
2025-03-23 20:48:07 -0500

    Include the pseudocode for the project
    I wrote the line-by-line algorithm for the game
    of "rock, paper, scissors" as comments in the
    JavaScript file. This algorithm is written for
    only one round of the game.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello World!');
+
+// Pseudocode Section
+
+// Line 1: When a user inputs a text of rock, paper, or scissors
+// Line 2: A random text of rock, paper, or scissors will be chosen for the computer
+// Line 3: If the user chose rock and the computer chose scissors, then the display will show "User won!"
+// Line 4: If the user chose scissors and the computer chose paper, then the display will show "User won!"
+// Line 5: If the user chose paper and the computer chose rock, then the display will show "User won!"
+// Line 6: If the user chose rock and the computer chose rock, then the display will show "It is a draw!"
+// Line 7: If the user chose scissors and the computer chose scissors, then the display will show "It is a draw!"
+// Line 8: If the user chose paper and the computer chose paper, then the display will show "It is a draw!"
+// Line 9: If the user chose rock and the computer chose paper, then the display will show "Computer won!"
+// Line 10: If the user chose scissors and the computer chose rock, then the display will show "Computer won!"
+// Line 11: If the user chose paper and the computer chose scissors, then the display will show "Computer won!"
