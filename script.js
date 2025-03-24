1b9371b9eba12ce1170d5bc8b935d500d10409f3
Gabriel-Debian <106034246+gmjohnson683@users.noreply.github.com>
2025-03-23 21:53:13 -0500

    Generate a response for the computer to play game
    I created a function that takes a randomly
    generated value and converts that to a "rock",
    "paper", or "scissors" response when evaluated
    by the conditional statements within said
    function. The output of the function becomes the
    computer's choice in the game of "rock, paper,
    scissors".

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-console.log('Hello World!');
-
 // Pseudocode Section
 
 // Line 1: When a user inputs a text of rock, paper, or scissors
 // Line 2: A random text of rock, paper, or scissors will be chosen for the computer
+
+function getComputerChoice(randomNumber) {
+    if(randomNumber < 33) {
+        return 'rock';
+    } else if (randomNumber > 67) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+let randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+
+let computerChoice = getComputerChoice(randomNumber);
+console.log(computerChoice);
+
 // Line 3: If the user chose rock and the computer chose scissors, then the display will show "User won!"
 // Line 4: If the user chose scissors and the computer chose paper, then the display will show "User won!"
 // Line 5: If the user chose paper and the computer chose rock, then the display will show "User won!"
