a2a857de4e081d13bdf6ff9e7e9f5f910b6b4a51
Gabriel-Debian <106034246+gmjohnson683@users.noreply.github.com>
2025-03-25 19:06:46 -0500

    Determine the winner of a round of rock, paper, scissors
    I developed all the conditional if statements that
    compares the user's choice and the computer's
    choice and determine who won that round of "rock,
    paper, scissors", if possible. Also, it will
    display that the round was a draw when the user's
    choice is the same as the computer's choice.
    
    The next version of the JavaScript file will
    include a function that adds a point to whoever
    won the round of "rock, paper, scissors". Also,
    the conditional statements will be consolidated
    to a simplified function.

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ let computerScore = 0;
 
 function getHumanChoice() {
     let userResponse = prompt('Which will you be choosing? Rock, paper, or scissors?');
-    let humanChoice = userResponse.toLowerCase();
-    return humanChoice;
+    return userResponse;
 }
 
-console.log( getHumanChoice() );
+const humanChoice = getHumanChoice();
+console.log(humanChoice);
 
 
 // Line 3: A random text of rock, paper, or scissors will be chosen for the computer
 
-function getComputerChoice(randomNumber) {
-    if(randomNumber <= 33) {
+function getComputerChoice(number) {
+    if(number <= 33) {
         return 'rock';
-    } else if (randomNumber => 66) {
+    } else if (number >= 66) {
         return 'paper';
     } else {
         return 'scissors';
@@ -29,17 +29,61 @@ function getComputerChoice(randomNumber) {
 }
 
 let randomNumber = Math.floor(Math.random() * 100);
-console.log( randomNumber );
+// console.log(randomNumber);
 
 const computerChoice = getComputerChoice(randomNumber);
 console.log(computerChoice);
 
 // Line 4: If the user chose rock and the computer chose scissors, then the display will show "User won!"
+
+if(humanChoice.toLowerCase() == 'rock' && computerChoice == 'scissors') {
+    console.log('Rock beats scissors! The user has won this round!');
+}
+
 // Line 5: If the user chose scissors and the computer chose paper, then the display will show "User won!"
+
+if(humanChoice.toLowerCase() == 'scissors' && computerChoice == 'paper') {
+    console.log('Scissors beat paper! The user has won this round!');
+}
+
 // Line 6: If the user chose paper and the computer chose rock, then the display will show "User won!"
+
+if(humanChoice.toLowerCase() == 'paper' && computerChoice == 'rock') {
+    console.log('Paper beats rock! The user has won this round!');
+}
+
 // Line 7: If the user chose rock and the computer chose rock, then the display will show "It is a draw!"
+
+if(humanChoice.toLowerCase() == 'rock' && computerChoice == 'rock') {
+    console.log('Rock neutralizes rock! It is a draw!')
+}
+
 // Line 8: If the user chose scissors and the computer chose scissors, then the display will show "It is a draw!"
+
+if(humanChoice.toLowerCase() == 'scissors' && computerChoice == 'scissors') {
+    console.log('Scissors neutralize scissors! It is a draw!')
+}
+
 // Line 9: If the user chose paper and the computer chose paper, then the display will show "It is a draw!"
+
+if(humanChoice.toLowerCase() == 'paper' && computerChoice == 'paper') {
+    console.log('Paper neutralizes paper! It is a draw!')
+}
+
 // Line 10: If the user chose rock and the computer chose paper, then the display will show "Computer won!"
+
+if(humanChoice.toLowerCase() == 'rock' && computerChoice == 'paper') {
+    console.log('Paper beats rock! The computer has won this round!')
+}
+
 // Line 11: If the user chose scissors and the computer chose rock, then the display will show "Computer won!"
+
+if(humanChoice.toLowerCase() == 'scissors' && computerChoice == 'rock') {
+    console.log('Rock beats scissors! The computer has won this round!')
+}
+
 // Line 12: If the user chose paper and the computer chose scissors, then the display will show "Computer won!"
+
+if(humanChoice.toLowerCase() == 'paper' && computerChoice == 'scissors') {
+    console.log('Scissors beat paper! The computer has won this round!')
+}
